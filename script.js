115263b61ecbbb7d73beef0d8dc6a51e3153c967
Jeff <101219079+Jixxin@users.noreply.github.com>
2022-04-13 13:07:41 -0700

    added pexel video API

--- script.js
+++ script.js
@@ -37,10 +37,11 @@ M.Autocomplete.init(ac, {
 searchTextEl = $('#autocomplete-input')
 searchBtnEl = $('#searchBtn')
 formBtnEl = $('#formBtn')
+var popularEl = $('#test')
 
+function locationLoad() {
 
-function wikiPull() {
-
+  popularEl.empty()
   var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + searchTextEl.val() + '&utf8=&format=json&origin=*'
 
 
@@ -66,33 +67,65 @@ function wikiPull() {
         .then(function (wikiData) {
           var pageKey = data.query.search[0].pageid
           console.log(wikiData.query.pages[pageKey].extract)
+
+
+
+          popularEl.append('<p class ="blue" >' + wikiData.query.pages[pageKey].extract + '</p>')
           // descriptionEl.text(wikiData.query.pages[pageKey].extract)
         })
     })
+  var apikey = '563492ad6f91700001000001762d088d5ead45459359fd5c62d83861'
+  fetch(`https://api.pexels.com/videos/search?query=` + searchTextEl.val(),
+    {
+      method: "GET",
+      headers: {
+
+        Accept: "application/json",
+        Authorization: apikey,     //use the apikey you have generated
+      },
+
+    }).then(function (pexelResponse) {
+      return pexelResponse.json()
+    })
+
+    .then(function (pexelVidData) {
+      console.log(pexelVidData)
+      var pexelURL = pexelVidData.videos[0].video_files[0].link
+      popularEl.append('<iframe width="560" height="315" src="' + pexelURL + '"frameborder="0" allowfullscreen></iframe>')
+    })
+
+
 }
+
+var imgOneEl = $('#img1')
 
 function saveForm() {
   console.log("working")
 
   var name = $('#name')
   var landmark = $('#place')
-  //var date = $('#date')
+  var date = $('#Date')
   var blog = $('#message')
 
-
+  console.log(date.val())
   console.log(name.val())
   console.log(landmark.val())
   console.log(blog.val())
 
   localStorage.setItem('name', name.val())
   localStorage.setItem('landmark', landmark.val())
+  localStorage.setItem('date', date.val())
   localStorage.setItem('blog', blog.val())
+
+  renderBlog()
+
 }
 
 function renderBlog() {
 
   var savednameEl = $('#saved-name')
   var savedlandmarkEl = $('#saved-landmark')
+  var saveddateEl = $('#saved-Date')
   var savedmessageEl = $('#saved-message')
 
   if (localStorage.getItem('name') !== null) {
@@ -100,6 +133,9 @@ function renderBlog() {
   }
   if (localStorage.getItem('landmark') !== null) {
     savedlandmarkEl.text(localStorage.getItem('landmark'))
+  }
+  if (localStorage.getItem('date') !== null) {
+    saveddateEl.text(localStorage.getItem('date'))
   }
   if (localStorage.getItem('blog') !== null) {
     savedmessageEl.text(localStorage.getItem('blog'))
@@ -109,4 +145,4 @@ function renderBlog() {
 
 renderBlog()
 formBtnEl.click(saveForm)
-searchBtnEl.click(wikiPull)
+searchBtnEl.click(locationLoad)
